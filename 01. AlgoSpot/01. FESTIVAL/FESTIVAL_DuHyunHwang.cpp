930464bcb8fa36feee470b080d0e2ace24af42cc
duhyun <doohh88@gmail.com>
2015-05-15 13:00:38 +0900

    정답
    정답

--- 01. AlgoSpot/01. FESTIVAL/FESTIVAL_DuHyunHwang.cpp
+++ 01. AlgoSpot/01. FESTIVAL/FESTIVAL_DuHyunHwang.cpp
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
+using namespace std;
 
 typedef struct TESTCASE{
 	int n; //대여할 수 있는 날들의 수
@@ -11,11 +13,11 @@ typedef struct TESTCASE{
 } TESTCASE;
 
 double findMinPrice(TESTCASE testcase){
-	int calcNum = testcase.l;
+	int calcNum = testcase.l;	//가수
 	int sum = 0;
 	double avg = 0;
 	int i = 0, j = 0;
-	while (calcNum < testcase.n){
+	while (calcNum <= testcase.n){	//가수 수 
 		for (i = 0; i <= testcase.n - calcNum; i++){
 			sum = 0;
 			for (j = 0; j < calcNum; j++){
@@ -29,7 +31,7 @@ double findMinPrice(TESTCASE testcase){
 				testcase.minPrice = avg;
 			}
 		}
-		calcNum++;
+		calcNum++; //가수 3->4->5->6
 	}
 	return testcase.minPrice;
 }
@@ -41,20 +43,22 @@ int main(){
 #endif //_WIN32
 
 	int	testcaseNum = 0;
-	scanf("%d", &testcaseNum);
-	TESTCASE testcase[100];
+	scanf("%d", &testcaseNum);	//몇개의 testcase?
+	TESTCASE testcase[100];	//testcase 저장할 배열 선언
 	int i = 0, j = 0;
-	for (i = 0; i < testcaseNum; i++){
-		scanf("%d", &testcase[i].n);
-		scanf("%d", &testcase[i].l);
-		for (j = 0; j < testcase[i].n; j++){
-			scanf("%d", &testcase[i].price[j]);
+	//read
+	for (i = 0; i < testcaseNum; i++){	//testcase개수 만큼 for문	2
+		scanf("%d", &testcase[i].n);	//day개수	6
+		scanf("%d", &testcase[i].l);	//l 가수 개수	3
+
+		for (j = 0; j < testcase[i].n; j++){	//	6번(day개수) for문
+			scanf("%d", &testcase[i].price[j]);	//	price 저장	i : testcase번호,	j=날짜
 		}
 	}
 
-	for (i = 0; i < testcaseNum; i++){
-		testcase[i].minPrice = findMinPrice(testcase[i]);
-		printf("%.11f\n", testcase[i].minPrice);
+	for (i = 0; i < testcaseNum; i++){	//testcase개수 만큼 for문
+		testcase[i].minPrice = findMinPrice(testcase[i]);	//testcase번호 마다 minprice 함수 실행
+		printf("%.11f\n", testcase[i].minPrice);	//출력
 	}
 	
 	return 0;
